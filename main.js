e993abd548f916ee712c3beede1d8ac9c94cda08
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2018-10-11 12:19:01 +0600

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const {app, BrowserWindow} = require('electron')
   
 function createWindow(){
-    let win = new BrowserWindow({width: 800, height: 600, show: false});
+    let win = new BrowserWindow({
+        width: 800, 
+        height: 600, 
+        show: false, 
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+    
     win.on('closed', () => {
       win = null
     });
@@ -10,14 +18,11 @@ function createWindow(){
 
     splashScreen = new BrowserWindow({width: 800, height: 600, frame: false, alwaysOnTop: true});
     splashScreen.loadURL(`file://${__dirname}/splash.html`);
-    win.loadURL("your_website_url");
+    win.loadURL("http://staff.netmow.com");
     win.once('ready-to-show', ()=>{
         splashScreen.destroy();
         win.show();
     });
-
-    
-  
 }
   
 app.on('ready', createWindow)
